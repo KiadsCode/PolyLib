876f1b17a6ae6a3219644bb25278b43560708098
KiadsCode <kirieshkaboom@gmail.com>
2023-07-28 14:45:07 +0700

    New Commit

--- polylib/LineBatch.cs
+++ polylib/LineBatch.cs
@@ -43,40 +43,6 @@ namespace PolyLib
             _effect.DiffuseColor = Color.White.ToVector3();
         }
 
-        public void DrawAngledLineWithRadians(Vector2 start, float length, float radians, Color color)
-        {
-            Vector2 offset = new Vector2(
-                (float)Math.Sin(radians) * length, //x
-                -(float)Math.Cos(radians) * length //y
-                );
-            Draw(start, start + offset, color);
-        }
-
-        public void DrawOutLineOfRectangle(Rectangle rectangle, Color color)
-        {
-            Draw(new Vector2(rectangle.X, rectangle.Y), new Vector2(rectangle.X + rectangle.Width, rectangle.Y), color);
-            Draw(new Vector2(rectangle.X, rectangle.Y), new Vector2(rectangle.X, rectangle.Y + rectangle.Height), color);
-            Draw(new Vector2(rectangle.X + rectangle.Width, rectangle.Y), new Vector2(rectangle.X + rectangle.Width, rectangle.Y + rectangle.Height), color);
-            Draw(new Vector2(rectangle.X, rectangle.Y + rectangle.Height), new Vector2(rectangle.X + rectangle.Width, rectangle.Y + rectangle.Height), color);
-        }
-
-        public void DrawOutLineOfTriangle(Vector2 point_1, Vector2 point_2, Vector2 point_3, Color color)
-        {
-            Draw(point_1, point_2, color);
-            Draw(point_1, point_3, color);
-            Draw(point_2, point_3, color);
-        }
-
-        float GetRadians(float angleDegrees)
-        {
-            return angleDegrees * ((float)Math.PI) / 180.0f;
-        }
-
-        public void DrawAngledLine(Vector2 start, float length, float angleDegrees, Color color)
-        {
-            DrawAngledLineWithRadians(start, length, GetRadians(angleDegrees), color);
-        }
-
         public void Draw(Vector2 start, Vector2 end, Color color)
         {
             _verticies.Add(new VertexPositionColor(new Vector3(start, 0f), color));
@@ -92,12 +58,6 @@ namespace PolyLib
                 else
                     Draw(polygon.Points[i], polygon.Points[0], color);
             }
-        }
-
-        public void Draw(Vector3 start, Vector3 end, Color color)
-        {
-            _verticies.Add(new VertexPositionColor(start, color));
-            _verticies.Add(new VertexPositionColor(end, color));
         }
 
         public void End()
